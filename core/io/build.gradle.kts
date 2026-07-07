f9dee11f1656a98aba7a810f03b94c0ccdf7fda2
niceyayale <niceyayale@users.noreply.github.com>
2026-07-07 18:30:43 +0000

    build(core:io): 创建 KMP 模块骨架

---
+++ core/io/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "17"
+        }
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+
+    sourceSets {
+        commonMain { dependencies { } }
+        commonTest {
+            dependencies {
+                implementation(libs.junit.jupiter.api)
+                implementation(libs.junit.jupiter.params)
+                runtimeOnly(libs.junit.jupiter.engine)
+            }
+        }
+        jvmMain { dependencies { } }
+        jvmTest { dependencies { } }
+    }
+}
